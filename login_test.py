a89fe566e14c9a9e94145ea3d78d0cdf0d67fb9f
Adrian Jade Manaday <38102918+adrianJADEveloper@users.noreply.github.com>
2023-08-23 16:48:59 +0000

    Updated in GitHub .

--- login_test.py
+++ login_test.py
@@ -1,6 +1,6 @@
 def jadeInfo(fName, lName, goal):
     return (
-        f"Hello world! My name is {fName} {lName} and One day! I will become a {goal}")
+        f"Hello world! My name is {fName} {lName} and One day! I will become a {goal}. UPDATED in the GitHub code repo IDE PLS")
 
 
 print(jadeInfo("Adrian Jade", "Manaday", "Full-Stack Software Engineer"))
